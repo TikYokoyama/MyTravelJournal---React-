ff5cfd7f66fd4c6f9bab291951d0d850df4e5845
TikYokoyama <tiaki.yokoyama97@gmail.com>
2022-06-23 19:15:48 -0300

    base projeto com card de viagem

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <nav>
+            <i className="nav--icon" class="fa-solid fa-earth-americas"></i>
+            <h3 className="nav--title">my travel journal.</h3>
+        </nav>
+    )
+}
